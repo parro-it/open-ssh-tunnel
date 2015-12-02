ed55a4966ae28a1ae2796dced12a63eb9c528600
Andrea Parodi <andrea@parro.it>
2015-12-02 10:50:50 +0100

    added cli WIP

---
+++ cli.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const argv = require('yargs').argv;
+const sshTunnel = require('./');
+const openTunnel = sshTunnel.openTunnel;
+
+
+const defaults = {
+  srcAddr: '127.0.0.1',
+  dstAddr: '127.0.0.1',
+  readyTimeout: 13000,
+  forwardTimeout: 13000,
+  localAddr: '127.0.0.1'
+};
+const options = Object.assign({}, defaults, argv);
+
+openTunnel(options)
+  .then(() => {
+    process.stdout.write('Tunnel opened\n');
+  })
+  .catch(err => process.stdout.write(err.message + '\n'));
